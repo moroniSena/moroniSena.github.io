7dc41b98b5e1400b0e2a87a6f0122c8aa6d33782
moroniSena <lucasmoronidesena@gmail.com>
2016-06-21 10:20:40 -0600

    TechTrack

---
+++ newAngular.js
@@ -0,0 +1,16 @@
+(function() {
+	var app = angular.module('store', []);
+
+	 app.controller("TabController", function() {
+    this.tab = 1;
+
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+  });
+
+})();
